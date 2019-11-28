33e9a1bf628337ac9ce503b0abdb3a506e162cbb
Arthur Borges <abrothguer@gmail.com>
2019-11-27 22:05:25 -0300

    Finished splitter implementation

--- splitter.py
+++ splitter.py
@@ -11,7 +11,7 @@ import cv2
 
 
 # Functions
-def get_frames(vpath, fpath, fps=-1, fprefix="img_"):
+def get_frames(vpath, fpath, fps=-1, seconds=1, fprefix="img_"):
     """Open a video file and divides it into various frames.
 
     Args:
@@ -29,9 +29,23 @@ def get_frames(vpath, fpath, fps=-1, fprefix="img_"):
     count = 0
 
     while success:
+        print(f"Saving on ~/{fpath}{fprefix}{count}.jpg")
         cv2.imwrite(f"{fpath}{fprefix}{count}.jpg", image)
         success, image = capture.read()
 
         count += 1
-        capture.set(1, count * fps)
+        capture.set(1, count * fps * seconds)
         print(f"Frame {count} read...")
+
+
+def main():
+    get_frames("data/v1.mp4", "data/imgs/", fprefix="imgv1_", seconds=10)
+    get_frames("data/v2.mp4", "data/imgs/", fprefix="imgv2_", seconds=10)
+    get_frames("data/v3.mp4", "data/imgs/", fprefix="imgv3_", seconds=10)
+    get_frames("data/v4.mp4", "data/imgs/", fprefix="imgv4_", seconds=10)
+    get_frames("data/v5.mp4", "data/imgs/", fprefix="imgv5_", seconds=10)
+    get_frames("data/v6.mp4", "data/imgs/", fprefix="imgv6_", seconds=10)
+
+
+if __name__ == "__main__":
+    main()
